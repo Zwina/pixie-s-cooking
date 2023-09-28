20ad022a0d17b83dea31985d1af261bc9a88581a
Marjorie Kehil <marjorie.kehil28@hotmail.com>
2023-09-29 00:19:43 +0200

    corrections nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -30,8 +30,8 @@ function NavScroll() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
+            // style={{ maxHeight: "100px" }}
+            // navbarScroll
           >
             <Nav.Link href="/">Accueil</Nav.Link>
 
